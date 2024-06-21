b48dc77773f9cfcebbc27eb4c75ca181216d0725
LeonSilva15 <l30silva.o@gmail.com>
2024-06-20 22:00:00 -0600

    feat: add method to get the FizzBuzz string

--- src/01.intro.js
+++ src/01.intro.js
@@ -7,3 +7,21 @@
 export function max(num1, num2) {
     return num1 > num2 ? num1 : num2;
 }
+
+/**
+ * Get Fizz if divisible by 3, Buzz if divisible by 5 and FizzBuzz if divisible by both
+ * @param {number} num number to be verified
+ * @returns string result
+ */
+export function fizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+    }
+    if (num % 3 === 0) {
+        return 'Fizz';
+    }
+    if (num % 5 === 0) {
+        return 'Buzz';
+    }
+    return num.toString();
+}
