4672b3af35e042403e9ffe4189bb0aeb3dba5535
LeonSilva15 <l30silva.o@gmail.com>
2024-06-20 22:00:00 -0600

    feat: add method to get the greater number

---
+++ src/01.intro.js
@@ -0,0 +1,9 @@
+/**
+ * Get the greater number
+ * @param {number} num1 first number
+ * @param {number} num2 second number
+ * @returns the greater of the two paramaters
+ */
+export function max(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
