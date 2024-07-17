75c1a972547c22d871a25e3bd5dcb6bc8c8cab8e
LeonSilva15 <l30silva.o@gmail.com>
2024-07-16 23:21:06 -0600

    feat: add getShippingInfo method

--- src/04.mocking.js
+++ src/04.mocking.js
@@ -1,4 +1,5 @@
 import { getExchangeRate } from './libs/currency';
+import { getShippingQuote } from './libs/shipping';
 
 /**
  * Get the new price in the specified currency
@@ -9,4 +10,15 @@ import { getExchangeRate } from './libs/currency';
 export function getPriceInCurrency(price, currency) {
     const rate = getExchangeRate('USD', currency);
     return price * rate;
+}
+
+/**
+ * Get the shipping info
+ * @param {string} destination 
+ * @returns shipping info
+ */
+export function getShippingInfo(destination) {
+    const quote = getShippingQuote(destination);
+    if (!quote) return 'Shipping Unavailable';
+    return `Shipping Cost: $${quote.cost} (${quote.estimatedDays} Days)`;
 }
