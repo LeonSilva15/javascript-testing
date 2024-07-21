d9a2ba27402709a858087d4376e6a9e53c5ef7f9
LeonSilva15 <l30silva.o@gmail.com>
2024-07-20 23:25:09 -0600

    feat: add getDiscount method

--- src/04.mocking.js
+++ src/04.mocking.js
@@ -85,3 +85,13 @@ export function isOnline() {
 
     return currentHour >= open && currentHour < close;
 }
+
+/**
+ * Get the christmas day discount
+ * @returns christmas discount if applicable
+ */
+export function getDiscount() {
+    const today = new Date();
+    const isChristmasDay = today.getMonth() === 11 && today.getDate() === 25;
+    return isChristmasDay ? 0.2 : 0;
+}
