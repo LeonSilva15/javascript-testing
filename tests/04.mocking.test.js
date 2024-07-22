896f847a4e1d1b6963fda045149e0f029566b236
LeonSilva15 <l30silva.o@gmail.com>
2024-07-21 23:29:20 -0600

    feat: demonstrate interaction testing

--- tests/04.mocking.test.js
+++ tests/04.mocking.test.js
@@ -2,9 +2,20 @@ import { vi, it, expect, describe, beforeEach } from 'vitest'
 import {
     getPriceInCurrency,
     getShippingInfo,
+    renderPage,
+    submitOrder
 } from '../src/04.mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
+import { trackPageView } from '../src/libs/analytics';
+import { charge } from '../src/libs/payment';
+
+
+vi.mock('../src/libs/currency');
+vi.mock('../src/libs/shipping');
+
+vi.mock('../src/libs/analytics');
+vi.mock('../src/libs/payment');
 
 // A Mock Function is a function that imitates the behavior of a real function
 // It's used to test in isolation
@@ -81,5 +92,59 @@ describe(getShippingInfo, () => {
 
         expect(result).toMatch('$10');
         expect(result.toLowerCase()).toMatch('(5 days)');
+    });
+});
+
+// Interaction Testing
+describe(renderPage, () => {
+    it('should return correct content', async () => {
+        const result = await renderPage();
+
+        expect(result.toLowerCase()).toMatch('content');
+    });
+
+    it('should call analytics', async () => {
+        await renderPage();
+
+        expect(trackPageView).toHaveBeenCalledWith('/home');
+    })
+});
+
+describe(submitOrder, () => {
+    const order = { totalAmount: 10 };
+    const creditCard = { creditCardNumber: '12345678' };
+
+    it('should handle charging in payment', async () => {
+        vi.mocked(charge).mockResolvedValue({status: 'success'});
+
+        await submitOrder(order, creditCard);
+
+        expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+    });
+
+    it('should return success when payment is successful', async () => {
+        vi.mocked(charge).mockResolvedValue({status: 'success'});
+
+        const result = await submitOrder(order, creditCard);
+
+        expect(result.success).toBe(true);
+        // or
+        expect(result).toEqual({success: true});
+    });
+
+    it('should handle the failed endpoint call', async () => {
+        vi.mocked(charge).mockResolvedValue({status: 'failed'});
+
+        const result = await submitOrder(order, creditCard);
+
+        expect(result.success).toBe(false);
+        // or
+        expect(result).toHaveProperty('success', false);
+        // or
+        expect(result).toMatchObject({success: false});
+        // or
+        expect(result).toMatchObject({success: false, error: 'payment_error'});
+
+        expect(result.error.toLowerCase()).toMatch('payment');
     });
 });
